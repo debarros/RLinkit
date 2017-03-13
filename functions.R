b90cc7a5e65f371ea3d81aa91483196f132c511e
debarros <pj.debarros@gmail.com>
2017-03-13 12:07:21 -0400

    functions.R - filled in library and source statements, including new, distinct class definition files

--- functions.R
+++ functions.R
@@ -1,1 +1,25 @@
 #functions.R
+
+# Load Libraries ####
+library("XML")
+library("RCurl")
+library("openssl")
+library("R6")
+
+# Source classes ####
+source("AdditionalClasses.R")
+source("LinkItClasses.R")
+source("DTermClass.R")
+source("ProgramClass.R")
+source("RosterClass.R")
+source("SchoolClass.R")
+source("StaffClass.R")
+source("StudentClass.R")
+source("TestAssignClass.R")
+source("TestResultClass.R")
+
+
+# Source functions and other stuff ####
+source("cred.R")
+source("httpBuilders.R")
+source("searchAPI.R")
