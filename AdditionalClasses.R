bac1efbdc43a033f36a831763943cbecb61acfcf
debarros <pj.debarros@gmail.com>
2017-03-13 13:07:21 -0400

    AdditionalClasses.R - in the definition of the Account class, combined the two urlStub elements into a list; defaulted puKey and priKey to PublicKey and PrivateKey (from cred.R); put getPubKey and getPriKey methods on 1 line each; changed the initialize method to determine the mode; created the getUrlStub method

--- AdditionalClasses.R
+++ AdditionalClasses.R
@@ -8,27 +8,25 @@ Account <- R6Class(
   
   private = list(
     pem.address = "",
-    urlStub.dev = "",
-    urlStub.prod = "",
+    urlStub = list("development" = "https://www.linkitdev.com/restapi/v1", 
+                   "production" = "https://www.linkit.com/restapi/v1"),
     mode = factor(x = integer(), levels = c("development", "production")),
-    pubKey = PublicKey,
-    priKey = PrivateKey
+    pubKey = PublicKey, #this should be fixed
+    priKey = PrivateKey #this should be fixed
   ),
   
   public = list(
     Name = NULL,
-    initialize = function(name = NA){
-      self$name = name
+    initialize = function(mode){
+      private$mode = mode
     },
     toString = function(val = NA){
       print("nothing here yet")
     },
-    getpubKey = function() {
-      return(private$pubKey)
-    },
-    getpriKey = function() {
-      return(private$priKey)
-    }
+    getPubKey = function() {return(private$pubKey)},
+    getPriKey = function() {return(private$priKey)},
+    getUrlStub = function(){return(private$urlStub[private$mode][[1]])}, #the [[1]] makes it return just an atomic, unnamed character value
+    getMode = function(){return(private$mode)}
     #make set functions for each thingy in private
     #make get functions for each thingy in private
   )
