6d4419a48af747c7853cc089b61edd537513e000
debarros <pj.debarros@gmail.com>
2017-03-13 13:09:30 -0400

    searchAPI.R - changed the variable url to url.complete; changed some comments and stuff

--- searchAPI.R
+++ searchAPI.R
@@ -8,23 +8,24 @@
 searchAPI = function(acct, handle, resource, parameters = list()){
   
   #Determine the relative url
-  url.relative = "/schools?verb=search" #this should be based on the actual resource needed using a table
+  url.resource = "/schools?verb=search" #this should be based on the actual resource needed using a table
   
-  #add any additional paramters specific to the resource
-  parameters = someFunction(parameters, resource)
+  #add any additional paramaters specific to the resource
+  #parameters = someFunction(parameters, resource)
   
   #determine the headers required
-  
   httpheaders = c(Accept="application/xml",'Content-Type' = "application/xml")
   
   #make the URL
-  url = URLbuilder(url.relative, acct, parameters) #get the url
+  url.complete = URLbuilder(url.resource, acct, parameters) #get the url
   
   #retrieve it
   response = getURI(url.complete, httpheader = httpheaders, curl = handle)
   
-  #parse it based on the the resource type
-  ret = someOtherFunction(response, resource)
+  #parse it based on the resource type
+  ret = response
+  #ret = someOtherFunction(response, resource)
+  
   
   return(ret)
 }
