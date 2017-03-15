276be338e78036b44f0d03752aea298fe5dcdac7
debarros <pj.debarros@gmail.com>
2017-03-15 14:39:42 -0400

    SummarizeResults.R - prevented TestID from becoming a factor variable; replaced Date column with EarliestNewScan and LatestNewScan columns

--- SummarizeResults.R
+++ SummarizeResults.R
@@ -7,12 +7,14 @@
 #   Date - the earliest ModifiedDate of a result associatied with that test
 #   Count - how many results there are for that test in the results frame
 SummarizeResults = function(ResultFrame){
-  Summarize = data.frame(TestID = unique(ResultFrame$TestID))
+  Summarize = data.frame(TestID = unique(ResultFrame$TestID), stringsAsFactors = F)
   Summarize$TestName = ResultFrame$TestName[match(Summarize$TestID, ResultFrame$TestID)]
-  Summarize$Date = as.POSIXlt(NA)
+  Summarize$EarliestNewScan = as.POSIXlt(NA)
+  Summarize$LatestNewScan = as.POSIXlt(NA)
   for (i in 1:nrow(Summarize)){
     Summarize$Count[i] = sum(ResultFrame$TestID == Summarize$TestID[i])
-    Summarize$Date[i] = min(ResultFrame$ModifiedDate[ResultFrame$TestID == Summarize$TestID[i]])
+    Summarize$EarliestNewScan[i] = min(ResultFrame$ModifiedDate[ResultFrame$TestID == Summarize$TestID[i]])
+    Summarize$LatestNewScan[i] = max(ResultFrame$ModifiedDate[ResultFrame$TestID == Summarize$TestID[i]])
   }
   Summarize = Summarize[order(-Summarize$Count), ]
   return(Summarize)
