df52bb9cce45f8e4a6842c6de79cf1f9984a0658
debarros <pj.debarros@gmail.com>
2017-03-15 08:47:56 -0400

    httepBuilders.R - in URLbuilder, set the url.stub to be pulled from the account; shortened the paramString section; moved the code to get the public key into the url.partial line; in makeTimeStamp, added some comments explaining the function, fixed the line to extract the minute

--- httpBuilders.R
+++ httpBuilders.R
@@ -23,23 +23,17 @@ curlSetOpt(.opts = Options, curl = LinkItHandle)
 URLbuilder = function(url.resource, acct, parameters = list()){
   
   #set the beginning of the url, which is usually "https://www.linkitdev.com/restapi/v1" or "https://www.linkit.com/restapi/v1" 
-  url.stub = "https://www.linkitdev.com/restapi/v1" #get it from acct
+  url.stub = acct$getUrlStub()
   
   #put together the parameter section of the url
-  if (length(parameters)>0){
-    paramString = paste0("&",paste(names(parameters), parameters, sep = "=", collapse = "&"))
-  } else {
-    paramString = ""
-  }
-  
-  #Set the PublicKey to be used as the AccessKey
-  akey = acct$getPubKey() #get it from acct
+  paramString = ""
+  if (length(parameters)>0){paramString = paste0("&",paste(names(parameters), parameters, sep = "=", collapse = "&"))}
   
   #get the current date and time, properly formatted
   tStamp = makeTimeStamp()
   
   #Build the basic url
-  url.partial = paste0(url.stub,url.resource, paramString, "&TimeStamp=", tStamp, "&AccessKey=", akey)
+  url.partial = paste0(url.stub,url.resource, paramString, "&TimeStamp=", tStamp, "&AccessKey=", acct$getPubKey())
   
   #Complete the url by appending the hashed version
   url.complete = paste0(url.partial, "&Tag=",sha1(url.partial, key = acct$getPriKey()))
@@ -57,15 +51,19 @@ URLbuilder = function(url.resource, acct, parameters = list()){
 
 # - - - - - - - - - - - - - #
 # makeTimeStamp ####
-
+# This function generates a time stamp formatted as necessary for the API call
+# It can't be created directly using a format statement on the Sys.time() for two reasons:
+#   1) The time has to be in UTC (aka GMT or Greenwich Mean Time)
+#   2) Leading zeroes must be stripped from every component
 makeTimeStamp = function(now = Sys.time()){
-  now = as.POSIXlt(now, tz = "GMT")
+  now = as.POSIXlt(now, tz = "GMT") #time zone conversion requires POSIXlt instead of the POSIXct object returned by Sys.time()
   month = as.integer(format(now, format = "%m"))
   day = as.integer(format(now, format = "%d"))
   year = as.integer(format(now, format = "%Y"))
   hour = as.integer(format(now, format = "%I"))
-  minute = as.integer(format(now, format = "%I"))
+  minute = as.integer(format(now, format = "%M"))
   second = as.integer(format(now, format = "%S"))
   meridian = format(now, format = "%p")
   return(paste0(month,"%2f", day,"%2f", year,"+", hour,"%3a", minute,"%3a", second,"+", meridian))
 }
+
