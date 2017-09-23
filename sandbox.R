4e06e13722ba49ac9c62111ff136657fe12080e6
debarros <pj.debarros@gmail.com>
2017-09-23 13:40:41 -0400

    sandbox.R - Added a section to check the results to see if any need review; Added a section to organized the data from the scanned tests document.

--- sandbox.R
+++ sandbox.R
@@ -25,10 +25,23 @@ myAccount$setPubKey(ProdKey$Public)
 
 # Get all the results since lastTime
 ResultFrame = RecentResults(acct = myAccount, handle = LinkItHandle, lastTime = lastTime)
+
+#-------------------------#
+#### Check the results ####
+#-------------------------#
+
+# See if there are any results that need to be resolved before they can be reported
+# Counts of results in each grading status
+summary(factor(ResultFrame$GradingStatus)) 
+
+# tests that need review
+unique(ResultFrame$TestName[ResultFrame$GradingStatus %in% c("Incomplete", "Multi-Mark")]) 
+
+# Sections that need review
+unique(apply(X = ResultFrame[ResultFrame$GradingStatus %in% c("Incomplete", "Multi-Mark"),c("CourseName", "Section")], MARGIN = 1, paste, collapse = " "))
+
+# Not really sure why the next line is necessary
 ResultFrame = ResultFrame[ResultFrame$ModifiedDate >= lastTime,]
-summary(ResultFrame$ModifiedDate)
-summary(ResultFrame$ResultDate)
-summary(ResultFrame$CreatedDate)
 
 # Summarize the results since lastTime
 ResultSummary = SummarizeResults(ResultFrame)
@@ -36,10 +49,10 @@ ResultSummary = SummarizeResults(ResultFrame)
 # store the date and time of the current run
 gs_edit_cells(ss = ScannedTests.url, ws = 2, input = Sys.time(), anchor = "A2") 
 
-
 #----------------------#
 #### Report section ####
 #----------------------#
+
 # Read in the TABLE (Test Address Book, LinkIt Edition)
 TAB = read.xlsx(xlsxFile = "J:/tests/2017-2018/TAB.xlsx")
 
@@ -80,10 +93,22 @@ if(nrow(ScannedTests) > 0){
 
 # Modify ScannedTests to include the newly scanned tests
 NewScannedTests = data.frame(Test = ResultSummary$TestName, Folder = TAB$Folder.location[match(ResultSummary$TestName,TAB$TestName)], Analyze = T, Update = F, Monitor = T)
-ScannedTests = rbind(ScannedTests, NewScannedTests)
+AllScannedTests = rbind(ScannedTests, NewScannedTests)
+UniqueScannedTests = AllScannedTests[!duplicated(AllScannedTests$Test),]
+for(i in 1:nrow(UniqueScannedTests)){
+  for(j in c("Analyze","Update","Monitor")){
+    UniqueScannedTests[1,j] = any(unlist(AllScannedTests[AllScannedTests$Test == UniqueScannedTests$Test[i],j]))
+  }
+}
+
+# This next line has not been tested
+# Remove from UniqueScannedTests any records that require no action
+# UniqueScannedTests = UniqueScannedTests[apply(X = UniqueScannedTests[,c("Analyze","Update","Monitor")], MARGIN = 1, FUN = any),]
+
+
 
 # Update the Scanned Tests document with the modified ScannedTests 
-gs_edit_cells(ss = ScannedTests.url, ws = 1, input = ScannedTests, anchor = "A1") # Start at A1 b/c the header row is also added
+gs_edit_cells(ss = ScannedTests.url, ws = 1, input = UniqueScannedTests, anchor = "A1") # Start at A1 b/c the header row is also added
 
 
 
@@ -98,4 +123,7 @@ gs_edit_cells(ss = ScannedTests.url, ws = 1, input = ScannedTests, anchor = "A1"
 # Either way, attach the score report xlsx file
 # When you send an analysis or update, delete the values in the Analyze or Update columns
 # When you do score monitoring, delete the values in the Monitor column
+
+
+
 
